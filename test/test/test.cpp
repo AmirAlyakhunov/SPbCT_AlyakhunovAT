4dd41bf8cc2a4fe57806c4272ebdc8d3d923b5d3
Amir <67748620+AmirAlyakhunov@users.noreply.github.com>
2020-11-03 16:55:17 +0300

    added test code

---
+++ test/test/test.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	FILE *fp;
+	fopen_s(&fp, "rs.txt", "r");
+	string text;
+
+	if (fp == NULL) {
+		cout << "File error";
+		exit(0);
+	}
+
+	int ch = fgetc(fp);
+	while (ch != EOF) {
+		text += (char)ch;
+		text += ' ';
+		ch = fgetc(fp);
+	}
+
+	fclose(fp);
+	cout << text << endl;
+}
